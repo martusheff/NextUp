f4c08997a7b7245b078ec6be34f6cbd5d4b7fec1
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-03 20:14:18 -0700

    Initial Commit

---
+++ NextUp/NextUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextUpApp.swift
+//  NextUp
+//
+//  Created by andronick martusheff on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NextUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
